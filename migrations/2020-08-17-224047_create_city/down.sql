8653eca664729e8624c6d562d2d62d73868b954a
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-18 01:28:58 +0200

    some sql

--- migrations/2020-08-17-224047_create_city/down.sql
+++ migrations/2020-08-17-224047_create_city/down.sql
@@ -1,2 +1,6 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE citys
+DROP TABLE d01_citys;
+DROP TABLE d02_time_zone_utc;
+DROP TABLE d03_time_zone_info;
+DROP TABLE d04_d02_d03;
+DROP TABLE d05_d01_d02;
--- migrations/2020-08-17-224047_create_city/up.sql
+++ migrations/2020-08-17-224047_create_city/up.sql
@@ -11,7 +11,20 @@ CREATE TABLE d02_time_zone_utc (
   name VARCHAR NOT NULL
 );
 
-CREATE TABLE d03_time_zone_citys (
+CREATE TABLE d03_time_zone_info (
+  id VARCHAR NOT NULL PRIMARY KEY,
+  offset FLOAT NOT NULL,
+  text VARCHAR NOT NULL
+);
+
+CREATE TABLE d04_d02_d03 (
+  d02_time_zone_utc_id VARCHAR NOT NULL,
+  d03_time_zone_info_id VARCHAR NOT NULL,
+  FOREIGN KEY(d02_time_zone_utc_id) REFERENCES d02_citys(id),
+  FOREIGN KEY(d03_time_zone_info_id) REFERENCES d03_time_zone_info(id)
+);
+
+CREATE TABLE d05_d01_d02 (
   d01_citys_id VARCHAR NOT NULL,
   d02_time_zone_utc_id VARCHAR NOT NULL,
   FOREIGN KEY(d01_citys_id) REFERENCES d01_citys(id),
