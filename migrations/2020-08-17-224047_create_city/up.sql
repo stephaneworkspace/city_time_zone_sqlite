4a0c220c1f6df390d142bf941a9219bdd2c2bfc3
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-18 01:33:26 +0200

    some sql, diesel don't support many to many without PK

--- migrations/2020-08-17-224047_create_city/up.sql
+++ migrations/2020-08-17-224047_create_city/up.sql
@@ -18,6 +18,7 @@ CREATE TABLE d03_time_zone_info (
 );
 
 CREATE TABLE d04_d02_d03 (
+  id VARCHAR NOT NULL PRIMARY KEY,
   d02_time_zone_utc_id VARCHAR NOT NULL,
   d03_time_zone_info_id VARCHAR NOT NULL,
   FOREIGN KEY(d02_time_zone_utc_id) REFERENCES d02_citys(id),
@@ -25,6 +26,7 @@ CREATE TABLE d04_d02_d03 (
 );
 
 CREATE TABLE d05_d01_d02 (
+  id VARCHAR NOT NULL PRIMARY KEY,
   d01_citys_id VARCHAR NOT NULL,
   d02_time_zone_utc_id VARCHAR NOT NULL,
   FOREIGN KEY(d01_citys_id) REFERENCES d01_citys(id),
