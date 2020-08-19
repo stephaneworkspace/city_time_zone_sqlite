c3714bd44dbf328d0ea9c7569a6034667ef19e3a
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-19 12:48:01 +0200

    new sql format

--- migrations/2020-08-17-224047_create_city/up.sql
+++ migrations/2020-08-17-224047_create_city/up.sql
@@ -8,13 +8,15 @@ CREATE TABLE d01_citys (
 
 CREATE TABLE d02_time_zone_utc (
   id VARCHAR NOT NULL PRIMARY KEY,
-  name VARCHAR NOT NULL
+  name VARCHAR NOT NULL,
+  UNIQUE (name)
 );
 
 CREATE TABLE d03_time_zone_info (
   id VARCHAR NOT NULL PRIMARY KEY,
   offset FLOAT NOT NULL,
-  text VARCHAR NOT NULL
+  text VARCHAR NOT NULL,
+  UNIQUE (text)
 );
 
 CREATE TABLE d04_link_d02_d03 (
@@ -22,7 +24,8 @@ CREATE TABLE d04_link_d02_d03 (
   d02_time_zone_utc_id VARCHAR NOT NULL,
   d03_time_zone_info_id VARCHAR NOT NULL,
   FOREIGN KEY(d02_time_zone_utc_id) REFERENCES d02_time_zone_utc(id),
-  FOREIGN KEY(d03_time_zone_info_id) REFERENCES d03_time_zone_info(id)
+  FOREIGN KEY(d03_time_zone_info_id) REFERENCES d03_time_zone_info(id),
+  UNIQUE (d02_time_zone_utc_id, d03_time_zone_info_id)
 );
 
 CREATE TABLE d05_link_d01_d02 (
@@ -30,5 +33,6 @@ CREATE TABLE d05_link_d01_d02 (
   d01_citys_id VARCHAR NOT NULL,
   d02_time_zone_utc_id VARCHAR NOT NULL,
   FOREIGN KEY(d01_citys_id) REFERENCES d01_citys(id),
-  FOREIGN KEY(d02_time_zone_utc_id) REFERENCES d02_time_zone_utc(id)
+  FOREIGN KEY(d02_time_zone_utc_id) REFERENCES d02_time_zone_utc(id),
+  UNIQUE (d02_citys_id, d02_time_zone_utc_id)
 );
